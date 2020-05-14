82082299f63992761e74c2ae3002dc70ce5e80f9
Dev2 <Gireesh@DESKTOP-STCMGSB>
2020-05-14 20:18:10 +0530

    Dev1 Modified the test.java file

--- DemoProj1/src/com/demo/test/Test.java
+++ DemoProj1/src/com/demo/test/Test.java
@@ -1,9 +1,13 @@
 package com.demo.test;
 
+import com.demo.beans.Utility;
+
 public class Test {
 
 	public static void main(String[] args) {
 
+		Utility u = new Utility();
+		System.out.println(u.getSystDate());
 	}
 
 }
